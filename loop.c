b05c8983fcddf2bd877cf492b733e194ee9fe8b2
Magomed Rabadanov <rabadanovm808@gmail.com>
2026-06-24 22:06:52 +0300

    исправил ошибки синтаксиса

--- loop.c
+++ loop.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    // for (float i = 10601; i >= 60; i /= 5)
-    //{
-    //     printf("%.2f\n", i);
-    // }
+    for (float i = 10601; i >= 60; i /= 5)
+    {
+        printf("%.2f\n", i);
+    }
 
     return 0;
 }
